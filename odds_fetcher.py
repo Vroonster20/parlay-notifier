84cebf684afcc226985985261f5da4b6ec381b87
Vroonster20 <jbvroon21@gmail.com>
2026-08-24 17:59:33 -0500

    Added a flatten_games function to reduce the output to contain only necessary info, implemented a get_prices function to help get the price/odds of home/away teams.

--- odds_fetcher.py
+++ odds_fetcher.py
@@ -3,6 +3,10 @@ import json
 import requests
 from pathlib import Path
 from dotenv import load_dotenv
+from datetime import datetime, timezone
+
+#TODO: check to see if snapshot_odds.json is correct with the
+#new flatten_games()
 
 load_dotenv()
 SNAPSHOT_PATH = "snapshot_odds.json"
@@ -11,12 +15,52 @@ def fetch_odds():
     run_mode = os.environ.get("RUN_MODE", "snapshot")
 
     if run_mode == "live":
-        return _fetch_live_odds()
+        raw = _fetch_live_odds()
     elif run_mode == "snapshot":
-        return _fetch_snapshot_odds()
+        raw = _fetch_snapshot_odds()
     else:
         raise ValueError(f"Unknown RUN_MODE: {run_mode}")
 
+    return flatten_games(raw)
+
+def flatten_games(raw_games):
+    flat = []
+    now = datetime.now(timezone.utc)
+    selected_bookmaker = "BetMGM"
+
+    for game in raw_games:
+        commence_time = datetime.fromisoformat(game["commence_time"]. replace("Z", "+00:00"))
+        
+        bookmakers = game.get('bookmakers', [])
+
+        has_selected_bookmaker = any(b.get('title') == selected_bookmaker for b in bookmakers)
+        if (commence_time > now) and (has_selected_bookmaker):
+            selected = next(b for b in bookmakers if b.get('title') == selected_bookmaker)
+            prices = get_prices(selected)
+            home_team = game["home_team"]
+            away_team = game["away_team"]
+            home_price = prices.get(game["home_team"])
+            away_price = prices.get(game["away_team"])
+
+            flat.append({
+                "home_team": home_team,
+                "away_team": away_team,
+                "home_price": home_price,
+                "away_price": away_price,
+                "commence_time": commence_time.isoformat(),
+            })
+        
+    return flat
+
+def get_prices(bookmaker):
+    info = {}
+    for market in bookmaker.get('markets', []):
+        if market.get('key') == 'h2h':
+            for outcome in market.get('outcomes', []):
+                team = outcome.get('name', 'Unknown')
+                price = outcome.get('price', 'N/A')
+                info[team] = price
+    return info
 
 def _fetch_live_odds():
     api_key = os.environ.get("THE_ODDS_KEY")

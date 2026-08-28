9da153b2a2927dc7db3f6f13cb53128d4e301295
Vroonster20 <jbvroon21@gmail.com>
2026-08-28 16:40:24 -0500

    Created build_safe_parlay function to take data from odds_fetcher.py and create a safe parlay. TODO: make parlay_builder.py more complex

--- parlay_builder.py
+++ parlay_builder.py
@@ -1,7 +1,52 @@
-def factorial(n):   
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+import os
+import json
+import requests
+from pathlib import Path
+from odds_fetcher import fetch_odds
 
-print(factorial(5))
+
+def build_safe_parlay(games=None, min_legs=2, max_legs=4, max_favorite_price=1.8):
+    if games is None:
+        games = fetch_odds()
+    picks = []
+
+    for match in games:
+        try:
+            home_price = float(match["home_price"])
+            away_price = float(match["away_price"])
+
+            if home_price < away_price:
+                favorite_team = (match["home_team"])
+                favorite_price = home_price
+            elif away_price < home_price:
+                favorite_team = (match["away_team"])
+                favorite_price = away_price
+            else:
+                continue
+
+            if favorite_price > max_favorite_price:
+                continue
+
+            picks.append({
+                "team": favorite_team,
+                "price": favorite_price
+            })
+
+        except KeyError as e:
+            print(f"Missing key {e}")
+        except ValueError:
+            print(f"Price values must be numeric.")
+        except Exception as e:
+            print(f"Unexpected error - {e}")
+
+    if len(selected_picks) < min_legs:
+        return []
+
+    sorted_picks = sorted(picks, key=lambda item: item["price"])
+    selected_picks = sorted_picks[:max_legs]
+    return selected_picks
+
+
+if __name__ == "__main__":
+    parlay = build_safe_parlay()
+    print(parlay)
